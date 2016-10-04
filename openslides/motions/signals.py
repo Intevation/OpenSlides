65a15cf559cc332d296265604dc90241eb3028c0
Emanuel Schuetze <emanuel@intevation.de>
2016-10-04 14:00:02 +0200

    CUSTOM: new OBK workflow

--- openslides/motions/signals.py
+++ openslides/motions/signals.py
@@ -13,92 +13,141 @@ def create_builtin_workflows(sender, **kwargs):
         # If there is at least one workflow, then do nothing.
         return
 
-    workflow_1 = Workflow.objects.create(name='Simple Workflow')
-    state_1_1 = State.objects.create(name=ugettext_noop('submitted'),
-                                     workflow=workflow_1,
-                                     allow_create_poll=True,
-                                     allow_support=True,
-                                     allow_submitter_edit=True)
-    state_1_2 = State.objects.create(name=ugettext_noop('accepted'),
-                                     workflow=workflow_1,
-                                     action_word='Accept',
-                                     recommendation_label='Acceptance',
-                                     css_class='success')
-    state_1_3 = State.objects.create(name=ugettext_noop('rejected'),
-                                     workflow=workflow_1,
-                                     action_word='Reject',
-                                     recommendation_label='Rejection',
-                                     css_class='danger')
-    state_1_4 = State.objects.create(name=ugettext_noop('not decided'),
-                                     workflow=workflow_1,
-                                     action_word='Do not decide',
-                                     recommendation_label='No decision',
-                                     css_class='default')
-    state_1_1.next_states.add(state_1_2, state_1_3, state_1_4)
-    workflow_1.first_state = state_1_1
-    workflow_1.save()
-
-    workflow_2 = Workflow.objects.create(name='Complex Workflow')
-    state_2_1 = State.objects.create(name=ugettext_noop('published'),
-                                     workflow=workflow_2,
-                                     allow_support=True,
+    # OBK workflow
+    workflow_3 = Workflow.objects.create(name='OBK')
+    state_3_1 = State.objects.create(name='eingereicht',
+                                     workflow=workflow_3,
                                      allow_submitter_edit=True,
-                                     dont_set_identifier=True)
-    state_2_2 = State.objects.create(name=ugettext_noop('permitted'),
-                                     workflow=workflow_2,
-                                     action_word='Permit',
-                                     recommendation_label='Permission',
+                                     allow_support=False,
+                                     allow_create_poll=False,
+                                     required_permission_to_see='motions.can_manage',
+                                     dont_set_identifier=True,
+                                     versioning=False)
+    state_3_2 = State.objects.create(name='geprüft',
+                                     workflow=workflow_3,
+                                     action_word='Geprüft',
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
+                                     dont_set_identifier=True,
+                                     versioning=False)
+    state_3_3 = State.objects.create(name='zugeordnet',
+                                     workflow=workflow_3,
+                                     action_word='Zugeordnet',
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
+                                     dont_set_identifier=False,
+                                     versioning=False)
+    state_3_4 = State.objects.create(name='Empfehlung der ABK liegt vor',
+                                     workflow=workflow_3,
+                                     action_word='Empfehlung der ABK liegt vor',
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
                                      allow_create_poll=True,
-                                     allow_submitter_edit=True,
+                                     dont_set_identifier=False,
                                      versioning=True,
-                                     leave_old_version_active=True)
-    state_2_3 = State.objects.create(name=ugettext_noop('accepted'),
-                                     workflow=workflow_2,
-                                     action_word='Accept',
-                                     recommendation_label='Acceptance',
+                                     leave_old_version_active=False)
+    state_3_10 = State.objects.create(name='angenommen',
+                                     workflow=workflow_3,
+                                     action_word='Annehmen',
+                                     recommendation_label='Annahme',
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
                                      versioning=True,
+                                     leave_old_version_active=False,
                                      css_class='success')
-    state_2_4 = State.objects.create(name=ugettext_noop('rejected'),
-                                     workflow=workflow_2,
-                                     action_word='Reject',
-                                     recommendation_label='Rejection',
+    state_3_11 = State.objects.create(name='angenommen in geänderter Fassung',
+                                     workflow=workflow_3,
+                                     action_word='Annehmen in geänderter Fassung',
+                                     recommendation_label='Annahme in geänderter Fassung',
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
                                      versioning=True,
+                                     leave_old_version_active=False,
+                                     css_class='success')
+    state_3_12 = State.objects.create(name='angenommen als Material zu Antrag',
+                                     workflow=workflow_3,
+                                     action_word='Annehmen als Material zu Antrag',
+                                     recommendation_label='Annahme als Material zu Antrag',
+                                     show_recommendation_extension_field=True,
+                                     show_state_extension_field=True,
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
+                                     versioning=True,
+                                     leave_old_version_active=False,
+                                     css_class='success')
+    state_3_13 = State.objects.create(name='angenommen als Material an',
+                                     workflow=workflow_3,
+                                     action_word='Annehmen als Material an',
+                                     recommendation_label='Annahme als Material an',
+                                     show_recommendation_extension_field=True,
+                                     show_state_extension_field=True,
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
+                                     versioning=True,
+                                     leave_old_version_active=False,
+                                     css_class='success')
+    state_3_14 = State.objects.create(name='angenommen in geänderter Fasssung als Material',
+                                     workflow=workflow_3,
+                                     action_word='Annehmen in geänderter Fassung als Material',
+                                     recommendation_label='Annahme in geänderer Fassung als Material',
+                                     show_recommendation_extension_field=True,
+                                     show_state_extension_field=True,
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
+                                     versioning=True,
+                                     leave_old_version_active=False,
+                                     css_class='success')
+    state_3_15 = State.objects.create(name='erledigt bei Annahme von Antrag',
+                                     workflow=workflow_3,
+                                     action_word='Erledigt durch Annahme von Antrag',
+                                     recommendation_label='Erledigt bei Annahme von Antrag',
+                                     show_recommendation_extension_field=True,
+                                     show_state_extension_field=True,
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
+                                     versioning=True,
+                                     leave_old_version_active=False,
+                                     css_class='default')
+    state_3_16 = State.objects.create(name='abgelehnt',
+                                     workflow=workflow_3,
+                                     action_word='Ablehnen',
+                                     recommendation_label='Ablehnung',
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
+                                     versioning=True,
+                                     leave_old_version_active=False,
                                      css_class='danger')
-    state_2_5 = State.objects.create(name=ugettext_noop('withdrawed'),
-                                     workflow=workflow_2,
-                                     action_word='Withdraw',
+    state_3_17 = State.objects.create(name='nicht befasst',
+                                     workflow=workflow_3,
+                                     action_word='Nicht befassen',
+                                     recommendation_label='Nichtbefassung',
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
                                      versioning=True,
+                                     leave_old_version_active=False,
                                      css_class='default')
-    state_2_6 = State.objects.create(name=ugettext_noop('adjourned'),
-                                     workflow=workflow_2,
-                                     action_word='Adjourn',
-                                     recommendation_label='Adjournment',
-                                     versioning=True,
+    state_3_20 = State.objects.create(name='zurückgezogen',
+                                     workflow=workflow_3,
+                                     action_word='Zurückziehen',
+                                     allow_submitter_edit=False,
+                                     allow_support=False,
+                                     allow_create_poll=False,
+                                     versioning=False,
                                      css_class='default')
-    state_2_7 = State.objects.create(name=ugettext_noop('not concerned'),
-                                     workflow=workflow_2,
-                                     action_word='Do not concern',
-                                     recommendation_label='No concernment',
-                                     versioning=True,
-                                     css_class='default')
-    state_2_8 = State.objects.create(name=ugettext_noop('refered to committee'),
-                                     workflow=workflow_2,
-                                     action_word='Refer to committee',
-                                     recommendation_label='Referral to committee',
-                                     versioning=True,
-                                     css_class='default')
-    state_2_9 = State.objects.create(name=ugettext_noop('needs review'),
-                                     workflow=workflow_2,
-                                     action_word='Needs review',
-                                     versioning=True,
-                                     css_class='default')
-    state_2_10 = State.objects.create(name=ugettext_noop('rejected (not authorized)'),
-                                      workflow=workflow_2,
-                                      action_word='Reject (not authorized)',
-                                      recommendation_label='Rejection (not authorized)',
-                                      versioning=True,
-                                      css_class='default')
-    state_2_1.next_states.add(state_2_2, state_2_5, state_2_10)
-    state_2_2.next_states.add(state_2_3, state_2_4, state_2_5, state_2_6, state_2_7, state_2_8, state_2_9)
-    workflow_2.first_state = state_2_1
-    workflow_2.save()
+    state_3_1.next_states.add(state_3_2, state_3_20)
+    state_3_2.next_states.add(state_3_3, state_3_20)
+    state_3_3.next_states.add(state_3_4, state_3_20)
+    state_3_4.next_states.add(state_3_10, state_3_11, state_3_12, state_3_13, state_3_14, state_3_15, state_3_16,
+            state_3_17, state_3_20)
+    workflow_3.first_state = state_3_1
+    workflow_3.save()
