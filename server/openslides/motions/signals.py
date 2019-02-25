8bdc887615d60fa574922acde0d8537f0c1f5aee
Emanuel Schütze <emanuel@intevation.de>
2019-02-25 09:20:13 +0100

    CUSTOM: custom workflow 'Gruene'

--- server/openslides/motions/signals.py
+++ server/openslides/motions/signals.py
@@ -13,135 +13,136 @@ def create_builtin_workflows(sender, **kwargs):
         # If there is at least one workflow, then do nothing.
         return
 
-    workflow_1 = Workflow(name="Simple Workflow")
+    # Workflow Grüne
+    workflow_1 = Workflow(name='Grüne')
     workflow_1.save(skip_autoupdate=True)
-    state_1_1 = State(
-        name="submitted",
+    state_1_0 = State(name='in Bearbeitung',
         workflow=workflow_1,
-        allow_create_poll=True,
-        allow_support=True,
+        restriction=[
+            "is_submitter",
+            "motions.can_manage"
+        ],
+        dont_set_identifier=True,
+        allow_submitter_edit=True
+    )
+    state_1_0.save(skip_autoupdate=True)
+    state_1_1 = State(name='eingereicht',
+        workflow=workflow_1,
+        restriction=[
+            "is_submitter",
+            "motions.can_manage"
+        ],
+        dont_set_identifier=True,
     )
     state_1_1.save(skip_autoupdate=True)
-    state_1_2 = State(
-        name="accepted",
-        workflow=workflow_1,
-        recommendation_label="Acceptance",
-        css_class="green",
-        merge_amendment_into_final=1,
+    state_1_2 = State(name='veröffentlicht',
+         workflow=workflow_1,
+         allow_support=True,
+         dont_set_identifier=True,
     )
     state_1_2.save(skip_autoupdate=True)
-    state_1_3 = State(
-        name="rejected",
-        workflow=workflow_1,
-        recommendation_label="Rejection",
-        css_class="red",
-        merge_amendment_into_final=-1,
+    state_1_3 = State(name='zugelassen',
+         workflow=workflow_1,
+         allow_support=True,
+         allow_create_poll=True,
     )
     state_1_3.save(skip_autoupdate=True)
-    state_1_4 = State(
-        name="not decided",
-        workflow=workflow_1,
-        recommendation_label="No decision",
-        css_class="grey",
-        merge_amendment_into_final=-1,
-    )
-    state_1_4.save(skip_autoupdate=True)
-    state_1_1.next_states.add(state_1_2, state_1_3, state_1_4)
-    workflow_1.first_state = state_1_1
-    workflow_1.save(skip_autoupdate=True)
 
-    workflow_2 = Workflow(name="Complex Workflow")
-    workflow_2.save(skip_autoupdate=True)
-    state_2_0 = State(
-        name="in progress",
-        workflow=workflow_2,
-        allow_submitter_edit=True,
-        dont_set_identifier=True,
+    # Empfehlungen der Antragskommission, inkl. passenden Status
+    state_1_10 = State(name='accepted',
+          workflow=workflow_1,
+          recommendation_label='Übernahme',
+          css_class='green',
+          merge_amendment_into_final=1,
     )
-    state_2_0.save(skip_autoupdate=True)
-    state_2_1 = State(
-        name="submitted",
-        workflow=workflow_2,
-        allow_support=True,
-        dont_set_identifier=True,
+    state_1_10.save(skip_autoupdate=True)
+    state_1_11 = State(name='angenommen nach Modifizierung',
+          workflow=workflow_1,
+          recommendation_label='modifizierte Übernahme',
+          show_recommendation_extension_field=True,
+          show_state_extension_field=True,
+          css_class='green',
+          merge_amendment_into_final=1,
     )
-    state_2_1.save(skip_autoupdate=True)
-    state_2_2 = State(
-        name="permitted",
-        workflow=workflow_2,
-        recommendation_label="Permission",
-        allow_create_poll=True,
+    state_1_11.save(skip_autoupdate=True)
+    state_1_12 = State(name='zurückgezogen wegen modifizierte Übernahme',
+          workflow=workflow_1,
+          recommendation_label='zurückgezogen wegen modifizierte Übernahme',
+          show_recommendation_extension_field=True,
+          show_state_extension_field=True,
+          css_class='grey',
+          merge_amendment_into_final=-1,
     )
-    state_2_2.save(skip_autoupdate=True)
-    state_2_3 = State(
-        name="accepted",
-        workflow=workflow_2,
-        recommendation_label="Acceptance",
-        css_class="green",
-        merge_amendment_into_final=1,
+    state_1_12.save(skip_autoupdate=True)
+
+    # Empfehlungen ohne passenden Status
+    state_1_a = State(name='strittig',
+         workflow=workflow_1,
+         recommendation_label='strittig',
+         css_class='yellow',
+         merge_amendment_into_final=-1,
     )
-    state_2_3.save(skip_autoupdate=True)
-    state_2_4 = State(
-        name="rejected",
-        workflow=workflow_2,
-        recommendation_label="Rejection",
-        css_class="red",
-        merge_amendment_into_final=-1,
+    state_1_a.save(skip_autoupdate=True)
+    state_1_b = State(name='Abstimmung',
+         workflow=workflow_1,
+         recommendation_label='Abstimmung',
+         css_class='yellow',
+         merge_amendment_into_final=-1,
     )
-    state_2_4.save(skip_autoupdate=True)
-    state_2_5 = State(
-        name="withdrawed",
-        workflow=workflow_2,
-        css_class="grey",
-        merge_amendment_into_final=-1,
+    state_1_b.save(skip_autoupdate=True)
+
+    # Beschlüsse
+    state_1_13 = State(name='erledigt',
+          workflow=workflow_1,
+          recommendation_label='erledigt',
+          show_recommendation_extension_field=True,
+          show_state_extension_field=True,
+          css_class='grey',
+          merge_amendment_into_final=-1,
     )
-    state_2_5.save(skip_autoupdate=True)
-    state_2_6 = State(
-        name="adjourned",
-        workflow=workflow_2,
-        recommendation_label="Adjournment",
-        css_class="grey",
-        merge_amendment_into_final=-1,
+    state_1_13.save(skip_autoupdate=True)
+    state_1_14 = State(name='zurückgezogen',
+          workflow=workflow_1,
+          recommendation_label='zurückgezogen',
+          css_class='grey',
+          merge_amendment_into_final=-1,
     )
-    state_2_6.save(skip_autoupdate=True)
-    state_2_7 = State(
-        name="not concerned",
-        workflow=workflow_2,
-        recommendation_label="No concernment",
-        css_class="grey",
-        merge_amendment_into_final=-1,
+    state_1_14.save(skip_autoupdate=True)
+    state_1_15 = State(name='Überweisung in Ausschuss',
+          workflow=workflow_1,
+          css_class='grey',
+          merge_amendment_into_final=-1,
     )
-    state_2_7.save(skip_autoupdate=True)
-    state_2_8 = State(
-        name="refered to committee",
-        workflow=workflow_2,
-        recommendation_label="Referral to committee",
-        css_class="grey",
-        merge_amendment_into_final=-1,
+    state_1_15.save(skip_autoupdate=True)
+    state_1_16 = State(name='nicht befasst',
+          workflow=workflow_1,
+          css_class='grey',
+          merge_amendment_into_final=-1,
     )
-    state_2_8.save(skip_autoupdate=True)
-    state_2_9 = State(
-        name="needs review",
-        workflow=workflow_2,
-        css_class="grey",
-        merge_amendment_into_final=-1,
+    state_1_16.save(skip_autoupdate=True)
+    state_1_17 = State(name='abgelehnt',
+          workflow=workflow_1,
+          recommendation_label='Ablehnung nach Abstimmung der Versammlung',
+          css_class='red',
+          merge_amendment_into_final=-1,
     )
-    state_2_9.save(skip_autoupdate=True)
-    state_2_10 = State(
-        name="rejected (not authorized)",
-        workflow=workflow_2,
-        recommendation_label="Rejection (not authorized)",
-        css_class="grey",
-        merge_amendment_into_final=-1,
+    state_1_17.save(skip_autoupdate=True)
+    state_1_18 = State(name='Sonstiges',
+          workflow=workflow_1,
+          recommendation_label='Sonstiges',
+          show_recommendation_extension_field=True,
+          show_state_extension_field=True,
+          css_class='grey',
+          merge_amendment_into_final=-1,
     )
-    state_2_10.save(skip_autoupdate=True)
-    state_2_0.next_states.add(state_2_1, state_2_5)
-    state_2_1.next_states.add(state_2_2, state_2_5, state_2_10)
-    state_2_2.next_states.add(
-        state_2_3, state_2_4, state_2_5, state_2_6, state_2_7, state_2_8, state_2_9
-    )
-    workflow_2.first_state = state_2_0
-    workflow_2.save(skip_autoupdate=True)
+    state_1_18.save(skip_autoupdate=True)
+
+    state_1_0.next_states.add(state_1_1)
+    state_1_1.next_states.add(state_1_2)
+    state_1_2.next_states.add(state_1_3)
+    state_1_3.next_states.add(state_1_10, state_1_11, state_1_12, state_1_13, state_1_14, state_1_15, state_1_16, state_1_17, state_1_18)
+    workflow_1.first_state = state_1_0
+    workflow_1.save(skip_autoupdate=True)
 
 
 def get_permission_change_data(sender, permissions, **kwargs):
