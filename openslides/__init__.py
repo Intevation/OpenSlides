c8ed9f7b8221e19a597ce6690b3abd81d0e4e12e
Emanuel Schütze <emanuel@intevation.de>
2019-08-27 16:03:10 +0200

    CUSTOM: 3.1-dgb-20190927-saml

--- openslides/__init__.py
+++ openslides/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "OpenSlides Team <support@openslides.org>"
 __description__ = "Presentation and assembly system"
-__version__ = '3.1-dgb-20190927'
+__version__ = '3.1-dgb-20190927-saml'
 __license__ = "MIT"
 __url__ = "https://openslides.com"
 
