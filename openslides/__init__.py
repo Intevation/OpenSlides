5ee3e78a15b760f8e4f7e540e17807c12d1eeb22
Emanuel Schütze <emanuel@intevation.de>
2020-08-19 09:47:06 +0200

    CUSTOM TEST: +pr5514

--- openslides/__init__.py
+++ openslides/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "OpenSlides Team <support@openslides.org>"
 __description__ = "Presentation and assembly system"
-__version__ = "3.2-gruene-20200715"
+__version__ = "3.2-gruene-20200715+pr5514"
 __license__ = "MIT"
 __url__ = "https://openslides.com"
 
