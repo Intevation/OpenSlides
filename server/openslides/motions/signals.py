98c28ac6e5fab2873ed753ba6b5c54198c7fca73
Emanuel Schütze <emanuel@intevation.de>
2021-01-11 19:41:00 +0100

    CUSTOM: CDU workflow

--- server/openslides/motions/signals.py
+++ server/openslides/motions/signals.py
@@ -143,6 +143,111 @@ def create_builtin_workflows(sender, **kwargs):
     workflow_2.first_state = state_2_0
     workflow_2.save(skip_autoupdate=True)
 
+    # CDU
+    workflow_3 = Workflow(name="CDU")
+    workflow_3.save(skip_autoupdate=True)
+    state_3_0 = State(
+        name="in Bearbeitung",
+        workflow=workflow_3,
+        restriction=[
+            "is_submitter",
+            "motions.can_manage"
+        ],
+        allow_submitter_edit=True,
+        dont_set_identifier=True,
+        merge_amendment_into_final=-1,
+    )
+    state_3_0.save(skip_autoupdate=True)
+    state_3_1 = State(
+        name="eingereicht",
+        workflow=workflow_3,
+        allow_support=True,
+        dont_set_identifier=True,
+        merge_amendment_into_final=-1,
+    )
+    state_3_1.save(skip_autoupdate=True)
+    state_3_2 = State(
+        name="zugelassen",
+        workflow=workflow_3,
+        allow_create_poll=True,
+        merge_amendment_into_final=-1,
+    )
+    state_3_2.save(skip_autoupdate=True)
+
+    state_3_10 = State(
+        name="angenommen",
+        workflow=workflow_3,
+        recommendation_label="Annahme",
+        css_class="green",
+        merge_amendment_into_final=1,
+    )
+    state_3_10.save(skip_autoupdate=True)
+    state_3_11 = State(
+        name="angenommen in geänderter Fassung",
+        workflow=workflow_3,
+        recommendation_label="Annahme in geänderter Fassung",
+        css_class="green",
+        merge_amendment_into_final=1,
+    )
+    state_3_11.save(skip_autoupdate=True)
+    state_3_12 = State(
+        name="abgelehnt",
+        workflow=workflow_3,
+        recommendation_label="Ablehnung",
+        css_class="red",
+        merge_amendment_into_final=-1,
+    )
+    state_3_12.save(skip_autoupdate=True)
+    state_3_13 = State(
+        name="erledigt",
+        workflow=workflow_3,
+        recommendation_label="Erledigt durch",
+        show_state_extension_field=True,
+        show_recommendation_extension_field=True,
+        css_class="grey",
+        merge_amendment_into_final=-1,
+    )
+    state_3_13.save(skip_autoupdate=True)
+    state_3_14 = State(
+        name="verwiesen",
+        workflow=workflow_3,
+        recommendation_label="Verweisung",
+        show_state_extension_field=True,
+        show_recommendation_extension_field=True,
+        css_class="grey",
+        merge_amendment_into_final=-1,
+    )
+    state_3_14.save(skip_autoupdate=True)
+    state_3_15 = State(
+        name="vertagt",
+        workflow=workflow_3,
+        recommendation_label="Vertagung",
+        css_class="grey",
+        merge_amendment_into_final=-1,
+    )
+    state_3_15.save(skip_autoupdate=True)
+    state_3_16 = State(
+        name="zurückgezogen",
+        workflow=workflow_3,
+        css_class="grey",
+        merge_amendment_into_final=-1,
+    )
+    state_3_16.save(skip_autoupdate=True)
+    state_3_17 = State(
+        name="verworfen (nicht zulässig)",
+        workflow=workflow_3,
+        css_class="grey",
+        merge_amendment_into_final=-1,
+    )
+    state_3_17.save(skip_autoupdate=True)
+    state_3_0.next_states.add(state_3_1, state_3_16)
+    state_3_1.next_states.add(state_3_2, state_3_16, state_3_17)
+    state_3_2.next_states.add(
+        state_3_10, state_3_11, state_3_12, state_3_13, state_3_14, state_3_15, state_3_16
+    )
+    workflow_3.first_state = state_3_0
+    workflow_3.save(skip_autoupdate=True)
+
 
 def get_permission_change_data(sender, permissions, **kwargs):
     """
